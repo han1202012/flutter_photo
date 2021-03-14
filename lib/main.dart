147478a1cfb22546e9093d1ecde2a4a270f78b3e
han1202012 <octopus_truth@163.com>
2021-03-14 14:33:38 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         //visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter 拍照示例'),
+      home: MyHomePage(title: '拍照示例'),
     );
   }
 }
